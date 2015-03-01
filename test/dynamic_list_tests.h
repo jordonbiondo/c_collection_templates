5e9f3646d52f851c0b1bce0e6ac3d98eebcf717a
jordonbiondo <biondoj@mail.gvsu.edu>
2015-03-01 09:31:33 -0500

    dyn list set test

--- test/dynamic_list_tests.h
+++ test/dynamic_list_tests.h
@@ -107,7 +107,21 @@ TEST dyn_list_get_test() {
 }
 
 TEST dyn_list_set_test() {
-  SKIP();
+  tdl* list = test_dyn_list_create(0);
+  test_dyn_list_add(list, 100);
+  test_dyn_list_add(list, 10);
+  test_dyn_list_add(list, 1);
+  ASSERT(test_dyn_list_get(list, 0) == 100);
+  ASSERT(test_dyn_list_get(list, 1) == 10);
+  ASSERT(test_dyn_list_get(list, 2) == 1);
+  test_dyn_list_set(list, 0, 99);
+  test_dyn_list_set(list, 1, 999);
+  test_dyn_list_set(list, 2, 9999);
+  ASSERT(test_dyn_list_get(list, 0) == 99);
+  ASSERT(test_dyn_list_get(list, 1) == 999);
+  ASSERT(test_dyn_list_get(list, 2) == 9999);
+  test_dyn_list_destroy(list, NULL);
+  PASS();
 }
 
 TEST dyn_list_add_test() {
