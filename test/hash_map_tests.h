81547a4db66cd499e9dd05c59d981612b5ecf68f
jordonbiondo <biondoj@mail.gvsu.edu>
2015-03-05 11:47:24 -0500

    finished up the hash map tests

--- test/hash_map_tests.h
+++ test/hash_map_tests.h
@@ -37,13 +37,8 @@ unsigned int test_str_hash_func(char* str) {
 }
 
 bool test_str_eq_func(char* a, char* b) {
-  while(*a && *b && *a == *b) {
-    a++;
-    b++;
-  }
-  return (*a == *b);
+  return strcmp(a, b) == 0;
 }
-
 
 define_hash_map(char*, int, test, test_str_hash_func, test_str_eq_func)
 
@@ -72,24 +67,162 @@ TEST prefix_hash_map_create () {
   PASS();
 }
 
+int destroyed_keys = 0;
+
+void my_key_destroyer(char* key) {
+  free(key);
+  destroyed_keys++;
+}
+
+int destroyed_data_sum = 0;
+
+void my_data_destroyer(int data) {
+  destroyed_data_sum += data;
+}
+
 TEST prefix_hash_map_destroy () {
-  SKIP();
+  hash_t* hash = test_hash_map_create(0);
+  char* key1 = malloc(sizeof(char) * 2);
+  strcpy(key1, "a");
+  char* key2 = malloc(sizeof(char) * 2);
+  strcpy(key1, "b");
+  test_hash_map_put(hash, key1, 1000);
+  test_hash_map_put(hash, key2, 1);
+  test_hash_map_destroy(hash, my_key_destroyer, my_data_destroyer);
+  ASSERT(destroyed_keys == 2);
+  ASSERT(destroyed_data_sum == 1001);
+  destroyed_keys = 0;
+  destroyed_data_sum = 0;
+  PASS();
 }
 
 TEST prefix_hash_map_put () {
-  SKIP();
+  size_t initial = 10;
+  hash_t* hash = test_hash_map_create(initial);
+  ASSERT(hash->private.capacity = initial);
+  ASSERT(hash->private.rehash_threshold == .75);
+  ASSERT(test_hash_map_put(hash, "1", 1) == true);
+  ASSERT(test_hash_map_put(hash, "2", 2) == true);
+  ASSERT(test_hash_map_put(hash, "3", 3) == true);
+  ASSERT(test_hash_map_put(hash, "4", 4) == true);
+  ASSERT(test_hash_map_put(hash, "5", 5) == true);
+  ASSERT(test_hash_map_put(hash, "6", 6) == true);
+  ASSERT(test_hash_map_put(hash, "7", 7) == true);
+  ASSERT(hash->private.capacity == initial);
+  ASSERT(test_hash_map_put(hash, "8", 8) == true);
+  ASSERT(hash->private.capacity > initial);
+  ASSERT(test_hash_map_put(hash, "9", 9) == true);
+  ASSERT(test_hash_map_put(hash, "10", 10) == true);
+  ASSERT(test_hash_map_count(hash) == 10);
+
+  test_hash_map_destroy(hash, NULL, NULL);
+
+  hash = test_hash_map_create(3);
+  hash->private.rehash_threshold = .5; // will rehash after second insert
+  ASSERT(test_hash_map_put(hash, "4", 3) == true);
+
+  with_spoofed_oom {
+    ASSERT(test_hash_map_put(hash, "4", 4) == true); // no rehash needed for existing key
+    ASSERT(test_hash_map_put(hash, "5", 5) == false); // rehash needed and should fail
+  }
+
+  ASSERT(test_hash_map_put(hash, "6", 6) == true);
+  ASSERT(test_hash_map_put(hash, "7", 7) == true);
+  ASSERT(test_hash_map_get(hash, "4") == 4);
+  ASSERT(test_hash_map_get(hash, "6") == 6);
+  ASSERT(test_hash_map_get(hash, "7") == 7);
+  test_hash_map_destroy(hash, NULL, NULL);
+  PASS();
 }
 
 TEST prefix_hash_map_get () {
-  SKIP();
+  hash_t* hash = test_hash_map_create(5);
+
+  for (char i = 0; i < 100; i++) {
+    char* s = malloc(sizeof(char) * 2);
+    s[0] = i;
+    s[1] = '\0';
+    test_hash_map_put(hash, s, i);
+  }
+
+  for (char i = 0; i < 100; i++) {
+    char s[] = {i, '\0'};
+    ASSERT(test_hash_map_get(hash, s) == i);
+  }
+
+  ASSERT(test_hash_map_get(hash, "not found") == (int)0);
+  test_hash_map_destroy(hash, my_key_destroyer, NULL);
+
+  PASS();
 }
 
 TEST prefix_hash_map_contains () {
-  SKIP();
+  hash_t* hash = test_hash_map_create(5);
+
+  for (char i = 1; i < 100; i++) {
+    char* s = malloc(sizeof(char) * 2);
+    s[0] = i;
+    s[1] = '\0';
+    test_hash_map_put(hash, s, i);
+  }
+
+  for (char i = 1; i < 100; i++) {
+    char s[] = {i, '\0'};
+    ASSERT(test_hash_map_contains(hash, s) == true);
+  }
+
+  for (char i = 1; i < 100; i++) {
+    char s[] = {i, i, '\0'};
+    ASSERT(test_hash_map_contains(hash, s) == false);
+  }
+
+  for (char i = 1; i < 100; i++) {
+    char s[] = {i, '\0'};
+    test_hash_map_remove(hash, s);
+  }
+
+  for (char i = 1; i < 100; i++) {
+    char s[] = {i, '\0'};
+    ASSERT(test_hash_map_contains(hash, s) == false);
+  }
+
+  test_hash_map_destroy(hash, my_key_destroyer, NULL);
+
+  PASS();
 }
 
 TEST prefix_hash_map_remove () {
-  SKIP();
+  hash_t* hash = test_hash_map_create(5);
+
+  for (char i = 1; i < 100; i++) {
+    char* s = malloc(sizeof(char) * 2);
+    s[0] = i;
+    s[1] = '\0';
+    test_hash_map_put(hash, s, i);
+  }
+
+  for (char i = 1; i < 100; i++) {
+    char s[] = {i, '\0'};
+    ASSERT(test_hash_map_contains(hash, s) == true);
+    hash_pair_t p = test_hash_map_remove(hash, s);
+    ASSERT(!p.empty);
+    ASSERT(strcmp(p.key, s) == 0);
+    ASSERT(p.data == i);
+    ASSERT(test_hash_map_contains(hash, s) == false);
+  }
+
+  for (char i = 1; i < 100; i++) {
+    char s[] = {i, '\0'};
+    ASSERT(test_hash_map_contains(hash, s) == false);
+  }
+
+  for (char i = 1; i < 100; i++) {
+    char s[] = {i, '\0'};
+    hash_pair_t p = test_hash_map_remove(hash, s);
+    ASSERT(p.empty);
+  }
+
+  PASS();
 }
 
 SUITE(hash_map) {
